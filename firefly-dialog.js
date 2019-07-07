e2c27258069099ddeffe0afeb37fd532f9a764e8
ishansubedi <ishan@appmozart.com>
2019-07-07 10:52:56 -0700

    testing wip

---
+++ firefly-dialog.js
@@ -0,0 +1,98 @@
+import { PolymerElement } from './node_modules/@polymer/polymer/polymer-element.js';
+import { html } from '@polymer/polymer/lib/utils/html-tag.js';
+import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
+
+// import './node_modules/@polymer/paper-dialog/paper-dialog.js';
+import './node_modules/@polymer/paper-button/paper-button.js';
+
+import {FireflyDialogMixin} from './firefly-dialog-mixin.js';
+
+/**
+ * `asp-fire-dialog` this component is responsible for displaying detail pages. These are 
+ * components that implement the AspComponentModelMixin and are tagged with the CSS class
+ * ".detail-page".
+ *
+ * @summary ShortDescription.
+ * @customElement
+ * @polymer
+ * @extends {Polymer.Element}
+ */
+class FireflyDialog extends FireflyDialogMixin(PolymerElement) {
+  static get template() {
+    return html`
+        <style>
+            :host {
+                display: block;
+                --dialog-content-height: 400px;
+                --dialog-title-color: rgba(15, 73, 169, 0.77);
+                --dialog-width: 400px;
+            }
+            paper-dialog{
+                --paper-dialog-background-color: white;
+                width: var(--dialog-width);
+            }
+            
+           paper-dialog > .dialog-content{
+               margin-left: 10px;
+               margin-right: 10px;
+               overflow-y: scroll;
+               height: var(--dialog-content-height);
+           }
+
+           paper-dialog > h2{
+                margin-left: 5px;
+                font-size: 1.2em;
+                color: var(--dialog-title-color);
+           }
+           paper-dialog > .buttons{
+               font-size: 1.0em;
+               color: #505050;
+           }
+ 
+        </style>
+
+        <paper-dialog no-cancel-on-outside-click="" horizontal-offset="300">
+            <h2>[[dialogLabel]]</h2>
+            
+            <div class="dialog-content">
+                <slot id="detail" select=".detail-page"></slot>
+            </div>
+            
+            
+            <div class="buttons">
+                <paper-button dialog-dismiss="">Cancel</paper-button>
+                <paper-button dialog-confirm="" autofocus="" on-tap="_addCard">Accept</paper-button>
+            </div>
+        </paper-dialog>
+`;
+  }
+
+  /**
+   * String providing the tag name to register the element under.
+   */
+  static get is() {
+      return 'firefly-dialog';
+  }
+
+  /**
+   * Instance of the element is created/upgraded. Use: initializing state,
+   * set up event listeners, create shadow dom.
+   * @constructor
+   */
+  constructor() {
+      super();
+  }
+
+  /**
+   * Use for one-time configuration of your component after local DOM is initialized. 
+   */
+  ready() {
+      super.ready();
+
+      afterNextRender(this, function() {
+          
+      });
+  }
+}
+
+window.customElements.define(FireflyDialog.is, FireflyDialog);
