37304ac2c27c738e1ea356f216900533611e0a21
asmin <sminmgr@gmail.com>
2020-12-23 10:03:57 +0545

    Add props to auto close on accept

--- firefly-dialog-mixin.js
+++ firefly-dialog-mixin.js
@@ -1,136 +1,145 @@
-import { FlattenedNodesObserver } from '@polymer/polymer/lib/utils/flattened-nodes-observer.js';
-import { matchesSelector } from '@polymer/polymer/lib/legacy/polymer.dom.js';
+import { FlattenedNodesObserver } from "@polymer/polymer/lib/utils/flattened-nodes-observer.js";
+import { matchesSelector } from "@polymer/polymer/lib/legacy/polymer.dom.js";
 
 /**
- * This mixin is implemented by all firebase-related dialogs.  
+ * This mixin is implemented by all firebase-related dialogs.
  * These dialogs have two modes: Add, and Edit which fire different
  * events depending on the mode they're in.
- * 
+ *
  * @polymerMixin
  * @mixinFunction
  */
-export const FireflyDialogMixin = (superclass) => class extends superclass {
+export const FireflyDialogMixin = (superclass) =>
+  class extends superclass {
+    static get properties() {
+      return {
+        /** The model for the dialog content */
+        model: {
+          type: Object,
+          value: {},
+        },
 
+        /** The label for the dialog. */
+        dialogLabel: {
+          type: String,
+          value: "",
+        },
 
-    static get properties(){
-        return {
-            
-            /** The model for the dialog content */
-            model:{
-                type: Object,
-                value: {}
-            },
+        /** Indicates whether the dialog is in 'Add' or 'Edit' mode. */
+        mode: {
+          type: String,
+          value: "",
+        },
 
-            /** The label for the dialog. */
-            dialogLabel:{
-                type: String,
-                value: ''
-            },
+        /**
+         * Indicates the type of card to be added/edited.  This is used when you have a single
+         * listener listening to the same type of event coming from multiple types of dialogs.
+         */
 
-            /** Indicates whether the dialog is in 'Add' or 'Edit' mode. */
-            mode:{
-                type: String,
-                value: ''
-            },
+        cardType: {
+          type: String,
+          value: "",
+        },
 
-            /** 
-             * Indicates the type of card to be added/edited.  This is used when you have a single
-             * listener listening to the same type of event coming from multiple types of dialogs.
-             */
+        positionTarget: {
+          type: String,
+          value: "",
+        },
 
-            cardType:{
-                type: String,
-                value: ''
-            },
-
-            positionTarget:{
-                type: String,
-                value: ''
-            },
-
-            fitInto:{
-                type: String,
-                value: ''
-            }
-            
-        }
+        fitInto: {
+          type: String,
+          value: "",
+        },
+        closeOnAccept: {
+          type: Boolean,
+          value: true,
+        },
+      };
     }
-
 
     /**
      * This function is usually called by the Accept button on a dialog, and is used to fire
      * a 'card-added' event.
      */
-     _addCard(){
-        let nodes = FlattenedNodesObserver.getFlattenedNodes(this);
-        let assignedNodes = nodes.filter( n => n.nodeType === Node.ELEMENT_NODE && matchesSelector(n, '.detail-page'));
-        let detail = assignedNodes[0];
-        this.model = detail.model;
+    _addCard() {
+      let nodes = FlattenedNodesObserver.getFlattenedNodes(this);
+      let assignedNodes = nodes.filter(
+        (n) =>
+          n.nodeType === Node.ELEMENT_NODE && matchesSelector(n, ".detail-page")
+      );
+      let detail = assignedNodes[0];
+      this.model = detail.model;
 
-        if(this.mode == 'Add' || this.mode == ''){
-            this.dispatchEvent(new CustomEvent('card-added', {
-                bubbles: true,
-                composed: true,
-                detail: {
-                    model: this.model,
-                    cardType: this.cardType
-                }
-            }));
-        }else{
-            this.dispatchEvent(new CustomEvent('card-updated', {
-                bubbles: true,
-                composed: true,
-                detail: {
-                    model: this.model,
-                    cardType: this.cardType
-                }
-            }));
-        }
-        
+      if (this.mode == "Add" || this.mode == "") {
+        this.dispatchEvent(
+          new CustomEvent("card-added", {
+            bubbles: true,
+            composed: true,
+            detail: {
+              model: this.model,
+              cardType: this.cardType,
+            },
+          })
+        );
+      } else {
+        this.dispatchEvent(
+          new CustomEvent("card-updated", {
+            bubbles: true,
+            composed: true,
+            detail: {
+              model: this.model,
+              cardType: this.cardType,
+            },
+          })
+        );
+      }
     }
-
 
     /**
-		 * This method is used to initialize the dialog's model before opening the dialog.
-		 * This method should be called whenever the dialog is used to create a 'new' object.
-		 */
-		init(){
-        this.model = {};
-        
-        let nodes = FlattenedNodesObserver.getFlattenedNodes(this);
-        let assignedNodes = nodes.filter( n => n.nodeType === Node.ELEMENT_NODE && matchesSelector(n, '.detail-page'));
-        let detail = assignedNodes[0];
+     * This method is used to initialize the dialog's model before opening the dialog.
+     * This method should be called whenever the dialog is used to create a 'new' object.
+     */
+    init() {
+      this.model = {};
 
-        detail.model = this.model;
+      let nodes = FlattenedNodesObserver.getFlattenedNodes(this);
+      let assignedNodes = nodes.filter(
+        (n) =>
+          n.nodeType === Node.ELEMENT_NODE && matchesSelector(n, ".detail-page")
+      );
+      let detail = assignedNodes[0];
 
-		}
-
-		/**
-		 * This method is responsible for displaying the dialog.
-		 */
-		open(){
-        this.mode = 'Edit';
-        let dialog = this.shadowRoot.querySelector("paper-dialog");
-        dialog.open();
-    }
-    
-    setModel(model){
-        this.model = model;
-
-        let nodes = FlattenedNodesObserver.getFlattenedNodes(this);
-        let assignedNodes = nodes.filter( n => n.nodeType === Node.ELEMENT_NODE && matchesSelector(n, '.detail-page'));
-        let detail = assignedNodes[0];
-        detail.model = this.model;
+      detail.model = this.model;
     }
 
-		/**
-		 * This variation of the open method initializes the model prior to opening the dialog.
-		 */
-		newOpen(){
-        this.mode = 'Add';
-        this.init();
-        let dialog = this.shadowRoot.querySelector("paper-dialog");
-        dialog.open();
-		}
-    
-}
+    /**
+     * This method is responsible for displaying the dialog.
+     */
+    open() {
+      this.mode = "Edit";
+      let dialog = this.shadowRoot.querySelector("paper-dialog");
+      dialog.open();
+    }
+
+    setModel(model) {
+      this.model = model;
+
+      let nodes = FlattenedNodesObserver.getFlattenedNodes(this);
+      let assignedNodes = nodes.filter(
+        (n) =>
+          n.nodeType === Node.ELEMENT_NODE && matchesSelector(n, ".detail-page")
+      );
+      let detail = assignedNodes[0];
+      detail.model = this.model;
+    }
+
+    /**
+     * This variation of the open method initializes the model prior to opening the dialog.
+     */
+    newOpen() {
+      this.mode = "Add";
+      this.init();
+      let dialog = this.shadowRoot.querySelector("paper-dialog");
+      dialog.open();
+    }
+  };
--- firefly-dialog.js
+++ firefly-dialog.js
@@ -1,12 +1,12 @@
-import { PolymerElement ,html} from '@polymer/polymer/polymer-element.js';
-import { afterNextRender } from '@polymer/polymer/lib/utils/render-status.js';
-import '@polymer/paper-dialog/paper-dialog.js';
-import '@polymer/paper-button/paper-button.js';
+import { PolymerElement, html } from "@polymer/polymer/polymer-element.js";
+import { afterNextRender } from "@polymer/polymer/lib/utils/render-status.js";
+import "@polymer/paper-dialog/paper-dialog.js";
+import "@polymer/paper-button/paper-button.js";
 
-import {FireflyDialogMixin} from './firefly-dialog-mixin.js';
+import { FireflyDialogMixin } from "./firefly-dialog-mixin.js";
 
 /**
- * `firefly-dialog` this component is responsible for displaying detail pages. These are 
+ * `firefly-dialog` this component is responsible for displaying detail pages. These are
  * components that implement the AspComponentModelMixin and are tagged with the CSS class
  * ".detail-page".
  *
@@ -18,56 +18,60 @@ import {FireflyDialogMixin} from './firefly-dialog-mixin.js';
 class FireflyDialog extends FireflyDialogMixin(PolymerElement) {
   static get template() {
     return html`
-        <style>
-            :host {
-                display: block;
-                --dialog-content-height: 400px;
-                --dialog-title-color: rgba(15, 73, 169, 0.77);
-                --dialog-width: 400px;
-            }
-            paper-dialog{
-                --paper-dialog-background-color: white;
-                width: var(--dialog-width);
-            }
-            
-           paper-dialog > .dialog-content{
-               margin-left: 10px;
-               margin-right: 10px;
-               overflow-y: scroll;
-               height: var(--dialog-content-height);
-           }
+      <style>
+        :host {
+          display: block;
+          --dialog-content-height: 400px;
+          --dialog-title-color: rgba(15, 73, 169, 0.77);
+          --dialog-width: 400px;
+        }
+        paper-dialog {
+          --paper-dialog-background-color: white;
+          width: var(--dialog-width);
+        }
 
-           paper-dialog > h2{
-                margin-left: 5px;
-                font-size: 1.2em;
-                color: var(--dialog-title-color);
-           }
-           paper-dialog > .buttons{
-               font-size: 1.0em;
-               color: #505050;
-           }
- 
-        </style>
+        paper-dialog > .dialog-content {
+          margin-left: 10px;
+          margin-right: 10px;
+          overflow-y: scroll;
+          height: var(--dialog-content-height);
+        }
 
-        <paper-dialog no-cancel-on-outside-click="" horizontal-offset="300">
-            <h2>[[dialogLabel]]</h2>
-            
-            <div class="dialog-content">
-                <slot id="detail" select=".detail-page"></slot>
-            </div>
-            <div class="buttons">
-                <paper-button dialog-dismiss="">Cancel</paper-button>
-                <paper-button dialog-confirm="" autofocus="" on-tap="_addCard">Accept</paper-button>
-            </div>
-        </paper-dialog>
-`;
+        paper-dialog > h2 {
+          margin-left: 5px;
+          font-size: 1.2em;
+          color: var(--dialog-title-color);
+        }
+        paper-dialog > .buttons {
+          font-size: 1em;
+          color: #505050;
+        }
+      </style>
+
+      <paper-dialog no-cancel-on-outside-click="" horizontal-offset="300">
+        <h2>[[dialogLabel]]</h2>
+
+        <div class="dialog-content">
+          <slot id="detail" select=".detail-page"></slot>
+        </div>
+        <div class="buttons">
+          <paper-button dialog-dismiss="">Cancel</paper-button>
+          <paper-button
+            dialog-confirm="[[closeOnAccept]]"
+            autofocus=""
+            on-tap="_addCard"
+            >Accept</paper-button
+          >
+        </div>
+      </paper-dialog>
+    `;
   }
 
   /**
    * String providing the tag name to register the element under.
    */
   static get is() {
-      return 'firefly-dialog';
+    return "firefly-dialog";
   }
 
   /**
@@ -76,18 +80,16 @@ class FireflyDialog extends FireflyDialogMixin(PolymerElement) {
    * @constructor
    */
   constructor() {
-      super();
+    super();
   }
 
   /**
-   * Use for one-time configuration of your component after local DOM is initialized. 
+   * Use for one-time configuration of your component after local DOM is initialized.
    */
   ready() {
-      super.ready();
+    super.ready();
 
-      afterNextRender(this, function() {
-          
-      });
+    afterNextRender(this, function () {});
   }
 }
 
